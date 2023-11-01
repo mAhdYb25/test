06c4182d5504094eec096f64a7dbf51f7c3bef84
unknown <mahdib25@gmail.com>
2023-11-01 13:38:45 +0330

    index modified for getting inputs

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php
 
 echo "Hi";
-
-
+$name = "mahdi";
+if (isset($_POST["Hi"])) {
+    $name = $_POST["name"];
+    $password = $_POST["password"];
+    $email = $_POST["email"];
+    $password2 = $_POST["password"];
+    $username = $_POST["username"];
+    if(empty($name) || empty($password) || empty($email))
+    {
+        echo "Hi ";
+        exit;
+    }
+}
 ?>
