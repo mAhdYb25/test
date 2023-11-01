eeac2350cf6873e03ebf17ecbfb8eb896d0be683
unknown <mahdib25@gmail.com>
2023-11-01 14:29:27 +0330

    index is made simpler

--- index.php
+++ index.php
@@ -2,18 +2,7 @@
 
 echo "Hi";
 $name = "mahdi";
-if (isset($_POST["Hi"])) {
-    $name = $_POST["name"];
-    $password = $_POST["password"];
-    $email = $_POST["email"];
-    $password2 = $_POST["password"];
-    $username = $_POST["username"];
-    if(empty($name) || empty($password) || empty($email))
-    {
-        echo "Hi ";
-        exit;
-    }
-}
+
 $a = 4;
 echo "this is huge";
 
