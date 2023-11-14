0ffc9afe952b9b3aa344c8a35fd542d1215a478b
mAhdYb25 <mahdib25@gmail.com>
2023-11-15 00:08:31 +0330

    bug fixes but memory error

--- kian.py
+++ kian.py
@@ -1,10 +1,21 @@
 n, src, dst = [int(i) for i in input().split(' ')]
 x=0
 y=0
-arr = [[0 for i in range(n)] for j in range(n)]
 state = 0
+flag = 0
+arr = [[0 for i in range(n)] for j in range(n)]
 for num in range(1, n**2 + 1):
     arr[y][x] = num
+    if num == src:
+        srcCord = y,x
+        if flag == 1:
+            break
+        flag = 1  
+    if num == dst:
+        dstCord = y,x
+        if flag == 1:
+            break
+        flag = 1  
     if state == 0:
         x += 1
         if x >= n:
@@ -46,5 +57,16 @@ for num in range(1, n**2 + 1):
             x += 1
             state = 0
 
-for i in range(n):
-    print(arr[i])
+deltaY = dstCord[0] - srcCord[0]
+deltaX = dstCord[1] - srcCord[1]
+result = ""
+if deltaX < 0 :
+    result += str(-deltaX) + " L\n"  
+elif deltaX>0:
+    result += str(deltaX) + " R\n"  
+
+if deltaY < 0 :
+    result += str(-deltaY) + " D"  
+elif deltaY>0:
+    result += str(deltaY) + " U"  
+print(result)
