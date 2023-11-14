ef64b4a296b1ca7566629252dfc7e376c2a1f777
mAhdYb25 <mahdib25@gmail.com>
2023-11-14 23:52:22 +0330

    finalized

--- kian.py
+++ kian.py
@@ -7,36 +7,44 @@ for num in range(1, n**2 + 1):
     arr[y][x] = num
     if state == 0:
         x += 1
-        if x >= num:
+        if x >= n:
             x -= 1
+            y += 1
             state = 1
         elif arr[y][x] != 0:
             x -= 1
+            y += 1
             state = 1            
     elif state == 1:
         y += 1
-        if y >= num:
+        if y >= n:
             y -= 1
+            x -= 1
             state = 2
         elif arr[y][x] != 0:
             y -= 1
+            x -= 1
             state = 2 
     elif state == 2:
         x -= 1
         if x < 0:
             x += 1
+            y -= 1
             state = 3
         elif arr[y][x] != 0:
             x += 1
+            y -= 1
             state = 3
     elif state == 3:
         y -= 1
         if y < 0:
             y += 1
+            x += 1
             state = 0
         elif arr[y][x] != 0:
             y += 1
+            x += 1
             state = 0
 
-for i in range(num):
-    print(arr[y])
+for i in range(n):
+    print(arr[i])
