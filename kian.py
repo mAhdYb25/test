114e71a9d98a9877d4bf19ac63ce281c9e74c950
mAhdYb25 <mahdib25@gmail.com>
2023-11-15 00:29:59 +0330

    memory leak fixed

--- kian.py
+++ kian.py
@@ -1,11 +1,11 @@
 n, src, dst = [int(i) for i in input().split(' ')]
-x=0
-y=0
+x=1
+y=1
+xLim = [1,n]
+yLim = [2,n]
 state = 0
 flag = 0
-arr = [[0 for i in range(n)] for j in range(n)]
 for num in range(1, n**2 + 1):
-    arr[y][x] = num
     if num == src:
         srcCord = y,x
         if flag == 1:
@@ -18,43 +18,23 @@ for num in range(1, n**2 + 1):
         flag = 1  
     if state == 0:
         x += 1
-        if x >= n:
-            x -= 1
-            y += 1
-            state = 1
-        elif arr[y][x] != 0:
-            x -= 1
-            y += 1
-            state = 1            
+        if x >= xLim[1]:
+            xLim[1] -= 1
+            state = 1           
     elif state == 1:
         y += 1
-        if y >= n:
-            y -= 1
-            x -= 1
+        if y >= yLim[1]:
+            yLim[1] -= 1
             state = 2
-        elif arr[y][x] != 0:
-            y -= 1
-            x -= 1
-            state = 2 
     elif state == 2:
         x -= 1
-        if x < 0:
-            x += 1
-            y -= 1
-            state = 3
-        elif arr[y][x] != 0:
-            x += 1
-            y -= 1
+        if x <= xLim[0]:
+            xLim[0] += 1
             state = 3
     elif state == 3:
         y -= 1
-        if y < 0:
-            y += 1
-            x += 1
-            state = 0
-        elif arr[y][x] != 0:
-            y += 1
-            x += 1
+        if y <= yLim[0]:
+            yLim[0] += 1
             state = 0
 
 deltaY = dstCord[0] - srcCord[0]
